6ea18f5ee69a4df77368ad1a40c7e9de93871bd1
Ivan <madritistaaa@gmail.com>
2017-05-21 19:47:30 +0300

    Add second hw

--- OS/Homework/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-all=$(find $1 -type f | wc -l)
-uniq=$(fdupes -r $1 | egrep '^$' | wc -l)
-allDuplicates=$(fdupes -r $1 | egrep -v '^$' | wc -l)
-res=$(($all-$allDuplicates+$uniq))
-
-echo "Count of unique files is $res"
-echo "$all"
