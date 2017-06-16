7e90f35a085cd8d03b9c82cb13f0e3db74f2ed51
Ivan Mladenov <mladenovv.ivan@gmail.com>
2017-06-16 11:25:32 +0300

    Os practice

---
+++ OS/practice/06.c.file.input.output/task1/main.c
@@ -0,0 +1,36 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int readFile;
+	int writeFile;
+	char c;
+
+	if (argc != 3)
+	{
+		exit(-1);
+	}
+
+	if ((readFile = open(argv[1], O_RDONLY)) == -1)
+	{
+		exit(-1);
+	}
+
+	if ((writeFile = open(argv[2], O_CREAT | O_WRONLY)) == -1)
+	{
+		close(readFile);
+		exit(-1);
+	}
+
+	while (read(readFile, &c, 1))
+	{
+		write(writeFile, &c, 1);
+	}
+
+	close(readFile);
+	close(writeFile);
+
+	exit(0);
+}
