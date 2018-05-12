a96da8fca823a1f94ac1d9c3d29e42f70d0cac90
Ivan <madritistaaa@gmail.com>
2018-05-12 18:53:25 +0300

    Add second task a part

--- OS-practicum/Homework/02/61950/competition.sh
+++ OS-practicum/Homework/02/61950/competition.sh
@@ -5,8 +5,12 @@ function=$2
 
 participants=$(ls -l $directory | tail -n +2 | rev | cut -d ' ' -f1 | rev)
 
-for participant in $participants;
-do
-  cat "$directory/$participant" | egrep -v "$participants"
-done
+#a
+if [ $function = "participants" ]; 
+then
+  for participant in $participants;
+  do
+    echo $participant
+  done
+fi
 
