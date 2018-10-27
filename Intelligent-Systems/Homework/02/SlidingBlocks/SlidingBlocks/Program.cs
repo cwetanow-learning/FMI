cae2448bec8a096c4a20bc01c6fa25abcea70ff5
Ivan <madritistaaa@gmail.com>
2018-10-27 17:35:08 +0300

    Add node class and enum for direction

--- Intelligent-Systems/Homework/02/SlidingBlocks/SlidingBlocks/Program.cs
+++ Intelligent-Systems/Homework/02/SlidingBlocks/SlidingBlocks/Program.cs
@@ -5,6 +5,44 @@ using System.Linq;
 
 namespace SlidingBlocks
 {
+	public enum Direction
+	{
+		Root,
+		Up,
+		Down,
+		Left,
+		Right
+	}
+
+	public class Node : IComparable<Node>
+	{
+		public Node(int h, int g, int[][] matrix)
+		{
+			this.H = h;
+			this.G = g;
+			this.F = this.H + this.G;
+
+			this.Matrix = matrix;
+		}
+
+		public int F { get; set; }
+		public int H { get; set; }
+		public int G { get; set; }
+
+		public int EmptyRow { get; set; }
+		public int EmptyCol { get; set; }
+
+		public Direction Direction { get; set; }
+		public Node Parent { get; set; }
+
+		public int[][] Matrix { get; set; }
+
+		public int CompareTo(Node other)
+		{
+			return this.F.CompareTo(other.F);
+		}
+	}
+
 	public class Program
 	{
 		public static void Main(string[] args)
