32cf633c48e94d1530d452d5bcb03776892b02df
Ivan <madritistaaa@gmail.com>
2018-10-27 15:29:02 +0300

    Parse input

--- Intelligent-Systems/Homework/02/SlidingBlocks/SlidingBlocks/Program.cs
+++ Intelligent-Systems/Homework/02/SlidingBlocks/SlidingBlocks/Program.cs
@@ -9,6 +9,19 @@ namespace SlidingBlocks
 	{
 		public static void Main(string[] args)
 		{
+			var n = int.Parse(Console.ReadLine());
+
+			var size = (int)Math.Sqrt(n + 1);
+
+			var matrix = new int[size][];
+
+			for (int i = 0; i < size; i++)
+			{
+				matrix[i] = Console.ReadLine()
+					.Split(' ')
+					.Select(int.Parse)
+					.ToArray();
+			}
 		}
 	}
 }
