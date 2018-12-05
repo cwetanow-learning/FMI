0c345f9da172987c557f6fdd908e4521730c0630
Ivan <madritistaaa@gmail.com>
2018-12-05 09:15:35 +0200

    Add class for frequency table

---
+++ Intelligent-Systems/Homework/07/NaiveBayesClassifier/FrequencyTable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace NaiveBayesClassifier
+{
+	public class FrequencyTable<TClass>
+	{
+		public FrequencyTable()
+		{
+			this.AggregatedData = new Dictionary<string, IDictionary<TClass, int>>();
+			this.TotalItems = 0;
+		}
+
+		public int TotalItems { get; set; }
+
+		public IDictionary<string, IDictionary<TClass, int>> AggregatedData { get; set; }
+	}
+}
